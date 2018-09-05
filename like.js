3c52082178e91dd6cb3c98b147dadcd9b196af06
David <sipos.david.daniel@gmail.com>
2018-09-05 10:38:40 +0200

    fix class names

--- like.js
+++ like.js
@@ -1,13 +1,13 @@
 function like(){
   try {
     document.getElementsByClassName('coreSpriteHeartOpen')[0].click(); // like button class
-    document.getElementsByClassName('_de018 coreSpriteRightPaginationArrow')[0].click(); // right arrow class
+    document.getElementsByClassName('coreSpriteRightPaginationArrow')[0].click(); // right arrow class
   } catch (e) {
-	// if picture liked just goint to the next picture
-    document.getElementsByClassName('_de018 coreSpriteRightPaginationArrow')[0].click(); // right arrow class
+    // if picture liked just goint to the next picture
+    document.getElementsByClassName('coreSpriteRightPaginationArrow')[0].click(); // right arrow class
   }
 }
-
+​
 var i = 1;
 var time = Math.floor((Math.random() * 5000) + 1000);
 var liker = setInterval(function(){
