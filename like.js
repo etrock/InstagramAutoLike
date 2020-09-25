c517ac93dc59bf7b87f21089e705e362b959a378
Sipos Dávid Dániel <sipos.david.daniel@gmail.com>
2020-09-26 01:00:20 +0200

    Fix like click

--- like.js
+++ like.js
@@ -1,6 +1,6 @@
 function like(){
   try {
-	  document.querySelectorAll("[aria-label='Tetszik']")[0].click() // onyl hungarian
+	  document.querySelectorAll("[aria-label='Tetszik']")[0].parentElement.click() // onyl hungarian
     document.getElementsByClassName('coreSpriteRightPaginationArrow')[0].click(); // right arrow class
   } catch (e) {
     // if picture liked just goint to the next picture
